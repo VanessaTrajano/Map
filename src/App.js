eae1598839c06dec37b57101d7b4bdf4dbd4c891
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2022-02-09 21:18:40 -0300

    Subindo a prática de map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Map extends Component {
+
+  state = {
+    personagens: [
+      {
+        nome: "Bruno",
+        filme: "Encanto"
+      },
+      {
+        nome: "Jim Hawkins",
+        filme: "Planeta do Tesouro"
+      },
+      {
+        nome: "Pumba",
+        filme: "Rei Leão"
+      },
+      {
+        nome: "Blu",
+        filme: "Rio"
+      },
+      {
+        nome: "Miguel",
+        filme: "Viva - A Vida é uma Festa"
+      },
+      {
+        nome: "José Bezerra",
+        filme: "Enrolados"
+      }
+    ]
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.personagens.map((item) => (
+          <div className="box">
+            <h2>Personagem: {item.nome}</h2>
+            <h2>Filme: {item.filme}</h2>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Map
